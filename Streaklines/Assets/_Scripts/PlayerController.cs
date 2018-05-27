9e396b1bd65189526bb9a6b1f76ac3444413dee1
Ian <>
2018-05-27 17:20:32 -0600

    Initial prototype jam

---
+++ Streaklines/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Tooltip("The force applied every physics update.")]
+    public float initialMovementImpulse = 1.0f;
+    public float movementInpulseIncreaseRatio = 1.01f;
+
+    [Header("Referenced Components")]
+    public Rigidbody2D playerRigidBody;
+
+    #region MonoBehaviour
+
+    private void FixedUpdate()
+    {
+        PlayerMovementUpdate();
+    }
+
+    private void PlayerMovementUpdate()
+    {
+
+    }
+
+#endregion
+}
